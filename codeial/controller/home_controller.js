f47119c1c14c7e3856c45e12fa6ac4bba38d7486
Tanya Nagi <TNagi@bpl.netlink.com>
2023-04-17 09:42:16 +0530

    added home.ejs with title

--- codeial/controller/home_controller.js
+++ codeial/controller/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req,res){
-    return res.end('controller is up')
+    return res.render('home',{
+        title:"Home"
+    });
 }
+
--- codeial/index.js
+++ codeial/index.js
@@ -2,15 +2,17 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-
 //use express router
 app.use('/',require('./routes/index'))
+
+//set up view engine
+app.set('view engine','ejs')
+app.set('views','./views')
 
 
 app.listen(port,function(err){
     if(err){
         console.log(`Error in porting:${err}`);
-        return
     }
     console.log(`Server running successfully on port:${port}`);
 
