ab447a0ec9255fd8ad9ca3075f039b0a98935d23
Tanya Nagi <TNagi@bpl.netlink.com>
2023-04-26 15:26:14 +0530

    Sign Up page created and data saved into databse

--- codeial/controller/users_controller.js
+++ codeial/controller/users_controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user")
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -20,7 +22,30 @@ module.exports.signIn = function(req,res){
 
 //get the sign-up data
 module.exports.create = function(req,res){
-    //todo later
+    if(req.body.password !== req.body.confirm_password){
+        return res.redirect('back')
+    }
+
+    User.findOne({email: req.body.email}).then((user)=>{
+        console.log(user,'-----user')
+        if(!user){
+            User.create(req.body).then(()=>{
+                return res.redirect('/users/sign-in')
+
+            }).catch(()=>{
+                console.log('error in creating user');
+
+            })
+        }
+        else{
+            return res.redirect('back')
+        }
+    }).catch(()=>{
+        if(err){
+            console.log('error in finding user that is logged in');
+        }
+        
+    })
 }
 
 //get the sign-in data
--- codeial/routes/users_router.js
+++ codeial/routes/users_router.js
@@ -11,5 +11,7 @@ router.get('/sign-up',usersController.signUp)
 //creating route for signin
 router.get('/sign-in',usersController.signIn)
 
+router.post('/create',usersController.create)
+
 
 module.exports = router;
