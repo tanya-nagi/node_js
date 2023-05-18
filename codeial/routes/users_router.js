4e3c9b40d4ced1db6ef8b5d285a2a2ed16161edc
Tanya Nagi <TNagi@bpl.netlink.com>
2023-05-18 21:41:54 +0530

    signout page implementation

--- codeial/routes/users_router.js
+++ codeial/routes/users_router.js
@@ -21,6 +21,8 @@ router.post('/create-session',passport.authenticate(
     {failureRedirect:'/users/sign-in'},
 ),usersController.createSession)
 
+router.get('/sign-out',usersController.destroySession)
+
 
 
 module.exports = router;
