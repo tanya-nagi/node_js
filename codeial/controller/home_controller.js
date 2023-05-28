5806ade14d7d963dbe715e5ec549ea76abf6558c
Tanya Nagi <TNagi@bpl.netlink.com>
2023-05-28 23:03:47 +0530

    Error while using populate function need to check on this

--- codeial/controller/home_controller.js
+++ codeial/controller/home_controller.js
@@ -1,8 +1,26 @@
-module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('use_name',25)
-    return res.render('home',{
-        title:"Home"
-    });
-}
+const Post = require("../models/post");
 
+module.exports.home = function (req, res) {
+  // console.log(req.cookies);
+  // res.cookie('use_name',25)
+
+//   Post.find({}).then((posts) => {
+//     console.log(posts, "---post");
+//     return res.render("home", {
+//       title: "Codeial | Home",
+//       posts: posts,
+//     });
+//   });
+
+//populate the user of each object
+Post.find({}).populate('user').then((posts)=>{
+    return res.render('home', {
+        title: "Codeial | Home",
+        posts:  posts
+    });
+})
+
+  // return res.render('home',{
+  //     title:"Home"
+  // });
+};
