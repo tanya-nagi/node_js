77512d10e26491e1cdfd757cee036b048faed31b
Tanya Nagi <TNagi@bpl.netlink.com>
2023-05-29 20:34:59 +0530

    checking if user is logged in then only user can post otherwise user only can see the post

--- codeial/controller/home_controller.js
+++ codeial/controller/home_controller.js
@@ -4,21 +4,22 @@ module.exports.home = function (req, res) {
   // console.log(req.cookies);
   // res.cookie('use_name',25)
 
-//   Post.find({}).then((posts) => {
-//     console.log(posts, "---post");
-//     return res.render("home", {
-//       title: "Codeial | Home",
-//       posts: posts,
-//     });
-//   });
+  Post.find({}).then((posts) => {
+    console.log(posts, "---post");
+    return res.render("home", {
+      title: "Codeial | Home",
+      posts: posts,
+    });
+  });
 
 //populate the user of each object
-Post.find({}).populate('user').then((posts)=>{
-    return res.render('home', {
-        title: "Codeial | Home",
-        posts:  posts
-    });
-})
+//need to cehck on this
+// Post.find({}).populate('user').then((posts)=>{
+//     return res.render('home', {
+//         title: "Codeial | Home",
+//         posts:  posts
+//     });
+// })
 
   // return res.render('home',{
   //     title:"Home"
--- codeial/routes/post_router.js
+++ codeial/routes/post_router.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
+const passport = require('passport')
 
 const postsController = require('../controller/posts_controller');
 
-router.post('/create',postsController.create)
+router.post('/create',passport.checkUserAuthentication,postsController.create)
 
 module.exports = router
