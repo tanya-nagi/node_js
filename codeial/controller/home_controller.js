3a6c2312431756b138b7c4ffc1ec33803003f3fb
Tanya Nagi <TNagi@bpl.netlink.com>
2023-06-18 11:11:28 +0530

    home controller async await

--- codeial/controller/home_controller.js
+++ codeial/controller/home_controller.js
@@ -1,7 +1,7 @@
 const Post = require("../models/post");
-const User = require('../models/user')
+const User = require("../models/user");
 
-module.exports.home = function (req, res) {
+module.exports.home = async function (req, res) {
   // console.log(req.cookies);
   // res.cookie('use_name',25)
 
@@ -14,27 +14,49 @@ module.exports.home = function (req, res) {
 
   //populate the user of each object
   //need to cehck on this
-  Post.find({})
-    .populate("user")
-    .populate({
-      path: "comments",
-      populate: {
-        path: "user",
-      },
-    })
-    .then((posts)=>{
-      User.find({}).then((users)=>{
-        return res.render('home', {
-          title: "Codeial | Home",
-          posts:  posts,
-          all_users: users
-      });
+  // Post.find({})
+  //   .populate("user")
+  //   .populate({
+  //     path: "comments",
+  //     populate: {
+  //       path: "user",
+  //     },
+  //   })
+  //   .then((posts)=>{
+  //     User.find({}).then((users)=>{
+  //       return res.render('home', {
+  //         title: "Codeial | Home",
+  //         posts:  posts,
+  //         all_users: users
+  //     });
 
-      })
-          
-      });
+  //     })
+
+  //     });
 
   // return res.render('home',{
   //     title:"Home"
   // });
+
+  try {
+    //using async await
+    let posts = await Post.find({})
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
+      });
+    let users = await User.find({});
+
+    return res.render("home", {
+      title: "Codeial | Home",
+      posts: posts,
+      all_users: users,
+    });
+  } catch (err) {
+    console.log('---err',err)
+    return
+  }
 };
